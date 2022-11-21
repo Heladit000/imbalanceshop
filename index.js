9d47302bda12a077bd0ab73373ff5d54a9889ef1
Heladit000 <nicolasestebanprieto44@gmail.com>
2022-11-21 11:56:49 -0500

    :art: Express hello World

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World! :D")
+//Express hello world
+
+const express = require("express");
+
+const App = express();
+
+const port = 3001;
+
+App.get("/", (req, res)=> {
+  //send this in petition
+  res.send("Hello World! :D");
+})
+
+App.listen(port, () => {
+  //eslint warn because its not good practise send
+  console.log(`The app running in ${port} port`);
+})
